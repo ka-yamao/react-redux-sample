87148e20e1ca0707ede27e35acc4b8bed8693069
山尾 和義 <kyamao@opendoor.co.jp>
2018-09-07 09:39:41 +0900

    stateの保存機能を実装

--- src/index.js
+++ src/index.js
@@ -2,30 +2,34 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App';
 import todo from './reducers';
 import { addTodo, toggleTodo, setVisibilityFilter } from './actions';
 // import { setVisibilityFilter } from './actions';
+import store, { persistor } from './configureStore';
 
-let store = createStore(todo);
+// let store = createStore(todo);
+
+// console.log('--------');
+// console.log(store.getState());
+// console.log('--------');
+
+// store.dispatch(addTodo('Hello React!!!!'));
+// store.dispatch(addTodo('Hello Redux!!!!'));
+// store.dispatch(addTodo('Hello Hoge!!!!'));
+// store.dispatch(toggleTodo(1));
+// store.dispatch(setVisibilityFilter('SHOW_COMPLETED'));
 
 console.log('--------');
-console.log(store.getState());
-console.log('--------');
-
-store.dispatch(addTodo('Hello React!!!!'));
-store.dispatch(addTodo('Hello Redux!!!!'));
-store.dispatch(addTodo('Hello Hoge!!!!'));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter('SHOW_COMPLETED'));
-
-console.log('--------');
-console.log(store.getState());
+// console.log(store.getState());
 console.log('--------');
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
